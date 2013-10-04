e61dc8156eede00f8898216aff800c63bffd8d20
Joshua Rountree <rountrjf@ucmail.uc.edu>
2013-10-04 11:10:38 -0400

    fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # This is a placeholder class.
 class hosts_prefpane {
-  package { 'Hosts.prefpane':
+  package { 'Hosts-1.3':
     provider => 'apple',
     source => 'https://github.com/downloads/specialunderwear/Hosts.prefpane/Hosts-1.3.pkg'
   }
--- spec/classes/hosts_prefpane_spec.rb
+++ spec/classes/hosts_prefpane_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'hosts_prefpane' do
   it do
-    should contain_package('Hosts.prefpane').with({
+    should contain_package('Hosts-1.3').with({
       :provider => 'pkgdmg',
       :source => 'https://github.com/downloads/specialunderwear/Hosts.prefpane/Hosts-1.3.pkg',
     })
