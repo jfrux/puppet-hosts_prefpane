cbfd5442676207b86c39ee8f9e0c94c28e0edb2b
Joshua Rountree <rountrjf@ucmail.uc.edu>
2013-10-04 10:26:04 -0400

    fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class hosts_prefpane {
+  package { 'Hosts':
+    provider => 'pkgdmg',
+    source => 'https://github.com/downloads/specialunderwear/Hosts.prefpane/Hosts-1.3.pkg'
+  }
 }
---
+++ spec/classes/hosts_prefpane_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+# Rename this file to classname_spec.rb
+# Check other boxen modules for examples
+# or read http://rspec-puppet.com/tutorial/
+describe 'hosts_prefpane' do
+  it do
+    
+  end
+end
